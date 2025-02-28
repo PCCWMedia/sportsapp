5c693d5afabaeb31f6d91a1633c4b9404f9e7437
Your Name <your.email@example.com>
2025-02-28 17:30:34 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,31 +7,33 @@ import AllFixtures from './pages/all-fixtures';
 import PLTable from './pages/plTable';
 import NavBar from './components/nav-bar';
 import Reels from './pages/reels';
-import Profile from './pages/Profile';
-
-function AppContent() {
-  const location = useLocation();
-
-  return (
-    <div className="app">
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/sportsapp/" element={<Home />} />
-          <Route path="/sportsapp/all-fixtures" element={<AllFixtures />} />
-          <Route path="/sportsapp/plTable" element={<PLTable />} />
-          <Route path="/sportsapp/reels" element={<Reels />} />
-          <Route path="/sportsapp/profile" element={<Profile />} />
-        </Routes>
-      </AnimatePresence>
-      <NavBar />
-    </div>
-  );
-}
-
+import Profile from './pages/Profile';  
 function App() {
   return (
-    <Router basename="/sportsapp">
-      <AppContent />
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={
+            <Home />
+          } />
+          <Route path="/all-fixtures" element={
+            <AllFixtures />
+          } />
+          <Route path="/plTable" element={
+            <PLTable />
+          } />
+          <Route path="/reels" element={
+            <Reels />
+          } />
+          <Route path="/profile" element={
+            <Profile />
+          } />
+          {/* Add more routes here as you create more pages */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+        <NavBar />
+      </div>
     </Router>
   );
 }
